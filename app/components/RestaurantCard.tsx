9194a4acac25613dcb6edce50ea4b66e93c594f9
ZacharyHangoc <hangoc.zachary@gmail.com>
2023-04-08 23:15:56 -0400

    seed reset test

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { RestaurantCardType } from "../page";
 import Price from "./Price";
-// import Stars from "./Stars";
+import Stars from "./Stars";
 
 interface Props {
   restaurant: RestaurantCardType;
@@ -15,7 +15,7 @@ export default function RestaurantCard({ restaurant }: Props) {
         <div className="p-1">
           <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
           <div className="flex items-start">
-            {/* <Stars reviews={restaurant.reviews} /> */}
+            <Stars reviews={restaurant.reviews} />
             <p className="ml-2">
               {restaurant.reviews.length} review
               {restaurant.reviews.length !== 1 && "s"}
